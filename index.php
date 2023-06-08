0832c493238760d5efb77a19b1cdd2c8573aeab7
Cristiano Ferreira <cristiano92ferreira@gmail.com>
2023-06-08 10:39:41 -0300

    alt1

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<html>
+  <head>
+    <title>REDE INFO</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    
 <?php
-  echo "987603456353";
-  ?>
+$dados = $_SERVER;
+echo '<pre>';
+
+$ip = isset($_SERVER["HTTP_X_REAL_IP"]) ? $_SERVER["HTTP_X_REAL_IP"] : "Indisponível";
+$rota = isset($_SERVER["HTTP_X_FORWARDED_FOR"]) ? $_SERVER["HTTP_X_FORWARDED_FOR"] : "Indisponível";
+$proxy = isset($_SERVER["REMOTE_ADDR"]) ? $_SERVER["REMOTE_ADDR"] : "Indisponível";
+
+echo "<br>IP DE ORIGEM: $ip";
+echo "<br>NÓS NA ROTA: $rota";
+echo "<br>ÚLTIMO NÓ: $proxy";
+
+echo '</pre>';
+
+?> 
+
+  </body>
+</html>
